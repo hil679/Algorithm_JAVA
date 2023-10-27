f6ab86398bf04e837b2d049a6927c190dd51aadc
hil679 <hil679922@gmail.com>
2023-10-27 20:35:21 +0900

    Hash: add second challenge and third correct
    - for문 없애기

--- src/main/java/org/example/hash/programmersHashPhoneBook.java
+++ src/main/java/org/example/hash/programmersHashPhoneBook.java
@@ -23,7 +23,7 @@ public class programmersHashPhoneBook {
             if (length > 1) {
                 for (int i = 0; i < length - 1; i++) {
                     String valueCreteria = keyList.get(i);
-                    for (int j = i + 1; j < length; i++) {
+                    for (int j = i + 1; j < length; j++) {
                         if (keyList.get(j).substring(0, valueCreteria.length()).equals(valueCreteria)) {
                             answer = false;
                             break;
@@ -38,5 +38,37 @@ public class programmersHashPhoneBook {
         }
 
         return answer;
+    }
+
+    public boolean solutio2(String[] phone_book) {
+        Arrays.sort(phone_book, (String a, String b) -> a.length() - b.length());
+        int length = phone_book.length;
+
+        for (int i = 0; i < length -1; i++) {
+            String valueCreteria = phone_book[i];
+            int valueCreteriaLength = valueCreteria.length();
+            for (int j = i + 1; j < length; j++) {
+                if (phone_book[j].indexOf(valueCreteria) == 0) {//indexof와 substring 속도 차이
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    //정답
+    public boolean solution3(String[] phone_book) {
+        Arrays.sort(phone_book); //문자열 길이가 아니라 문자열을 int로 바꿨을 때 그 숫자의 크기대로 sort됨(사전순)
+        //sort하면 phone_book[i]가 phone_book[i+1과 i+2]의 접두어일수는 있다.
+        //그러나 phone_book[i+1]이 phone_book[i+2]의 접두어가 아닌데 phone_book[i]가 phone_book[i+2]의 접두어일 수는 없다.
+        int length = phone_book.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            if(phone_book[i+1].startsWith(phone_book[i])) {//indexof와 substring 속도 차이
+                return false;
+            }
+        }
+        return true;
     }
 }
