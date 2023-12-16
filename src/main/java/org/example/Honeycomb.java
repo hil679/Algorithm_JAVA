3eeb05656dd89e9bcf4cf4327ba85e33b1623310
hil679 <hil679922@gmail.com>
2023-12-16 10:43:01 +0900

    설명 주석 추가

--- src/main/java/org/example/Honeycomb.java
+++ src/main/java/org/example/Honeycomb.java
@@ -1,15 +1,17 @@
 package org.example;
 
 import java.util.Scanner;
-
+/*
+    외곽의 벌집이 늘어나는 개수 규칙을 구한다(등차수열)
+    1, 6, 12... 이렇게 줄마다 개수를 구하면 a2부터 6씩 늘어난다.
+ */
 public class Honeycomb {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int answer = 1;
-        while(n > 1) {
+        int answer = 1; // 처음 시작 지점을 무조건 포함하기 위해 1부터 시작
+        while(n > 1) { // 위에서 처음 시작 지점의 개수는 포함했음으로 처음 시작(1) 전까지 6n씩 빼가며 몇번의 줄을 넘어왔는지 확인
             n -= 6 * answer;
-            answer++;
         }
         System.out.println(answer);
     }
--- src/main/java/org/example/P1157.java
+++ src/main/java/org/example/P1157.java
@@ -2,40 +2,45 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Scanner;
-
+/*
+    1. 무조건 대문자로 만든다.
+    2. 단어의 알파벳을 순회하여 개수를 센다. 이때 가장 많이 나온 알파벳과 알파벳 개수를 업데이트한다.
+    3. map에서 value값(나온 각 알파벳 개수)을 순회하며 max값이 중복되는지 확인한다.
+    4. max값의 개수가 1이상이면 ?, 아니면 변수에 저장해둔 max알파벳을 출력한다.
+ */
 public class P1157 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
-        word = word.toUpperCase();
+        word = word.toUpperCase(); // 대문자로 바꾸기
 
         HashMap<Character, Long> alphabets = new HashMap<>();
-        long max = 0;
-        char maxChar = 0;
+        long max = 0; //최댓값
+        char maxChar = 0; //최댓값에 해당하는 알파벳
 
         for(char c : word.toCharArray()) {
-            if(alphabets.containsKey(c))
+            if(alphabets.containsKey(c)) // 이미 개수 센 알파벳이면 넘김
                 continue;
-            long cnt = word.chars().filter(character -> c == character).count();
-            alphabets.put(c, cnt);
+            long cnt = word.chars().filter(character -> c == character).count(); // 알파벳 개수 세기
+            alphabets.put(c, cnt); // 알파벳과 나온 개수 저장
 
-            if(cnt > max){
+            if(cnt > max){ // 새로운 알파벳의 개수와 기존 max알파벳 개수 비교 후 업데이트
                 max = cnt;
                 maxChar = c;
             }
         }
 
         long cnt = 0;
-        for(long i : alphabets.values()) {
+        for(long i : alphabets.values()) { //map에서 value값(나온 각 알파벳 개수)을 순회하며 max값이 중복되는지 확인
             if(i == max) {
                 cnt++;
                 if(cnt > 1)
                     break;
             }
         }
-        if(cnt == 1) {
+        if(cnt == 1) { //  max값의 개수가 1이면 변수에 저장해둔 max알파벳을 출력
             System.out.println(maxChar);
-        } else {
+        } else { // max값의 개수가 1이상이면 ?
             System.out.println("?");
         }
 
