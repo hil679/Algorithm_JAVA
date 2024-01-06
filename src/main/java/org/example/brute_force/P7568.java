eb13225874b881bf246f7334d6e8c73bd02847f2
hil679 <hil679922@gmail.com>
2024-01-06 12:31:52 +0900

    완전탐색: 키랑 몸무게 하나씩 모든 학생 비교 - 주석 추가

--- src/main/java/org/example/brute_force/P7568.java
+++ src/main/java/org/example/brute_force/P7568.java
@@ -2,7 +2,11 @@ package org.example.brute_force;
 
 import java.util.Scanner;
 /*
-키나 몸무게 둘 중 하나라도 같으면 덩치 비교가 안 된다는 것을 유의해야한다.
+* 키나 몸무게 둘 중 하나라도 같으면 덩치 비교가 안 된다는 것을 유의해야한다.
+
+새로 학생이 들어올 때마다 기존 학생과 비교하며 순위를 변동한다.
+무게를 비교하고 무게가 큰 학생이 키도 크다면 다른 학생의 순위를 높인다.
+무게와 키, 둘 중의 하나라도 같거나 무게와 키가 엇갈려서 순위 비교가 안된다면 순위 변동은 없다.
  */
 public class P7568 {
     public static void main(String[] args) {
@@ -15,12 +19,12 @@ public class P7568 {
             students[i] = s;
             for(int j = 0; j < i; j++) {
                 if(s.weight > students[j].weight) { // 새로운 학생의 무게가 클 때
-                    if(s.height > students[j].height) {
-                        students[j].rank++;
+                    if(s.height > students[j].height) { //새로운 학생의 키도 크다면
+                        students[j].rank++; // 기존학생 등수 올리기
                     }
                 } else if(s.weight < students[j].weight){ //이미 입력 받은 학생의 무게가 더 클 때
-                    if(s.height < students[j].height) {
-                        s.rank++;
+                    if(s.height < students[j].height) { // 기존 학생의 키도 크다면
+                        s.rank++; // 새로운 학생 등수 올리기
                     }
                 }
             }
@@ -47,10 +51,3 @@ public class P7568 {
         }
     }
 }
-
-
-//88 186
-//60 175
-//58 183
-//55 185
-//46 155
