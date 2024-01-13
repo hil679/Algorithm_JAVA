0523eac611895a4eca9c83f025771d5606b33625
hil679 <hil679922@gmail.com>
2024-01-13 23:40:48 +0900

    임스와 함께하는 미니게임: 집합 이용

--- src/main/java/org/example/P25757.java
+++ src/main/java/org/example/P25757.java
@@ -16,10 +16,13 @@ public class P25757 {
         String[] gameInfo = bf.readLine().split(" ");
         int n = Integer.valueOf(gameInfo[0]);
 
+        //어차피 중복 안되니까 다 add하고 set에서 알아서 중복 저장하지 않음
         while (n > 0) {
             names.add(bf.readLine());
             n--;
         }
+
+        //중복되지 않은 총 사람을 한 게임당 한께할 수 있는 사람의 수로 나눈 몫만큼 게임을 진행할 수 있다.
         if(gameInfo[1].equals("Y")) {
             System.out.println(names.size());
         } else if(gameInfo[1].equals("F")) {
