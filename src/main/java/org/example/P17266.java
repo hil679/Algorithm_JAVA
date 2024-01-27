5695934d179ab1ff93adf723ae4200b9203a542f
hil679 <hil679922@gmail.com>
2024-01-27 23:00:35 +0900

    어두운 굴다리: 이진탐색_높이 빨리 찾기

--- src/main/java/org/example/P17266.java
+++ src/main/java/org/example/P17266.java
@@ -3,20 +3,20 @@ package org.example;
 import java.util.Scanner;
 
 public class P17266 {
-    static int[] Arr;
-    static int N,M;
+    static int[] arr;
+    static int m,n;
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        N =sc.nextInt(); //굴다리의 길이
-        M =sc.nextInt(); //설치할 가로수 갯수.
-        Arr = new int[M];// 가로등이 설치된 지점 입력 받기
+        n =sc.nextInt();
+        m =sc.nextInt();
+        arr = new int[m];
 
-        for(int i = 0; i < M; i++) {
-            Arr[i] = sc.nextInt();
+        for(int i = 0; i < m; i++) { // 가로등이 설치된 지점 입력 받기
+            arr[i] = sc.nextInt();
         }
 
         int left = 1;
-        int right = N;
+        int right = n;
         int result = 0;
 
         while(left <= right) {
@@ -32,27 +32,22 @@ public class P17266 {
         }
         System.out.println(result);
     }
-    static boolean possible(int target) {
+    static boolean possible(int height) {
+        //0 - 10, 2 4, 5
         int prev = 0; // 이전 가로등이 비춘 마지막 지점, 0 지점부터 모두 비춰주어야 하기 때문에 0부터 시작
 
-        for(int i = 0; i < Arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) { // 모든 가로등 범위 확인 필요하기 때문에 전체 가로등 설치 폭 순회
                     /*
                      * 가로등이 있는 지점에서 비출 수 있는 높이(target)을 빼면 가로등이 비추는 최소값을 알 수가 있습니다.
-                     * 이 최소값을 기준으로 가로등이 빈곳없이 다 비추는지 조건을 확인합니다.
                      */
-            if(Arr[i] - target <= prev) {
-                prev = Arr[i] + target;
-                        // Arr[i] + target을 하게되면 가로등이 다시 비춰야만 하는 최소값을 리턴할 수 있습니다.
-            } else { return false; }
-                    /*
-                     * 가로등의 시작 지점에서 높이(target)만큼 비춘 곳이
-                     * 이전 가로등이 마지막으로 비춘 곳에 도달하지 못하면 모든 지점을 비출수 없습니다.
-                     */
+            if(arr[i] - height <= prev) { // 비출 수 있는 끝 지점 <= 이전에 비췄던 최대 끝 지점
+                prev = arr[i] + height; // arr[i] + target을 하게되면 가로등이 다시 비춰야만 하는 최소값
+            } else {
+                return false;
+            }
         }
-                /*
-                 * 마지막 지점도 가로등이 비출 수 있는지 확인해야 하기 때문에
-                 * 마지막 가로등이 비출 수 있는 끝 지점에서 굴다리의 끝 좌표를 뺐을 때 0보다 작거나 같아야 마지막 지점도 비춰집니다.
-                 */
-        return N - prev <= 0;
+        return n - prev <= 0;
+        //마지막 지점(n)도 가로등이 비출 수 있는지 확인 필요
+        // 비춰야 하는 끝 지점 - 비출 수 있는 끝 지점 <= 0 ( 비출 수 있는 끝 범위가 더 커야하니까)
     }
 }
