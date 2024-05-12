8b68ba561dea13e3fd4f062267a4dc62d0d3c82e
hil679 <hil679922@gmail.com>
2024-05-13 00:18:27 +0900

    bfs: A와B - target에서 start로 찾기(거꾸로 탐색)[SUCCESS]

--- src/main/java/org/example/dp/P12919.java
+++ src/main/java/org/example/dp/P12919.java
@@ -1,17 +1,18 @@
 package org.example.dp;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class P12919 {
     public static boolean isMatch = false;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
-        String target = scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String target = br.readLine();
+        String s = br.readLine();
 
-        dfs(s, target, "A");
-        dfs(s, target, "B");
+        dfs(s, target);
 
         if (isMatch) {
             System.out.println(1);
@@ -21,18 +22,19 @@ public class P12919 {
 
     }
 
-    public static void dfs(String s, String target, String option) {
-        StringBuilder sb = new StringBuilder(s); // 속도: String < StringBuffer < StringBuilder
-        sb.append(option);
-        String newString = sb.toString();
-        if (option == "B") {
-            newString = sb.reverse().toString();
+    public static void dfs(String s, String target) {
+        if (s.length() == target.length()) {
+            if(target.equals(s)){
+                isMatch = true;
+            }
+            return;
         }
-        if (newString.length() < target.length()) {
-            dfs(newString, target, "A");
-            dfs(newString, target, "B");
-        } else if(target.equals(newString)){
-            isMatch = true;
+        if (s.substring(s.length() - 1).equals("A")) {
+            dfs(s.substring(0, s.length() - 1), target);
+        }
+        if (s.startsWith("B")){
+            StringBuilder sb = new StringBuilder(s.substring(1)); // 속도: String < StringBuffer < StringBuilder
+            dfs(sb.reverse().toString(), target);
         }
     }
 
