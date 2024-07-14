98b52e144d1383c20653b09e1777dd3e951208a7
hil679 <hil679922@gmail.com>
2024-07-14 22:50:54 +0900

    fix: init value for start idx

--- src/main/java/org/example/dp/P2169.java
+++ src/main/java/org/example/dp/P2169.java
@@ -27,19 +27,22 @@ public class P2169 { //need to retry
         // 첫 줄
         for(int j = 2; j <= M; j++) dp[1][j] = mars[1][j]+ dp[1][j-1];
 
-
+dp[0][0] = 0;
         for(int i = 2; i <= N; i++) {
 
-            temp[0][0] = dp[i-1][1];
-            for(int j = 1; j <= M; j++) {
+            temp[0][1] = dp[i-1][1] + mars[i][1]; // 가장 왼쪽은 바로 위에서 옴
+            //왼쪽 / 위 비교
+            for(int j = 2; j <= M; j++) {
                 temp[0][j] = Math.max(temp[0][j-1], dp[i-1][j])+ mars[i][j];
             }
 
-            temp[1][M+1] = dp[i-1][M];
-            for(int j = M; j >= 1; j--) {
+            // 오른쪽 / 위 비교
+            temp[1][M] = dp[i-1][M] + mars[i][M]; //가장 오른쪽은 바로 위에서 옴
+            for(int j = M - 1; j >= 1; j--) {
                 temp[1][j] = Math.max(temp[1][j+1], dp[i-1][j]) + mars[i][j];
             }
 
+            //왼 / 오 비1
             for(int j = 1; j <= M; j++) {
                 dp[i][j] = Math.max(temp[0][j], temp[1][j]);
             }
