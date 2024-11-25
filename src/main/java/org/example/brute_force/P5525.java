29858e1407b2cab694e7528f221d9141b7910686
hil679 <hil679922@gmail.com>
2024-11-25 23:08:12 +0900

    [5525-string compare] using kmf algorithm

--- src/main/java/org/example/brute_force/P5525.java
+++ src/main/java/org/example/brute_force/P5525.java
@@ -1,2 +1,34 @@
-package org.example.brute_force;public class P5525 {
+package org.example.brute_force;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P5525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(bf.readLine());
+        int m = Integer.valueOf(bf.readLine());
+        String s = bf.readLine();
+
+        int ansCnt = 0;
+        int n_cnt = 0;
+        for (int i = 1; i < m - 1;){
+            if (s.charAt(i) == 'O' && s.charAt(i+1) == 'I'){
+                n_cnt++;
+                if (n_cnt == n){
+                    if (s.charAt(i - 2*n + 1) == 'I') {
+                        ansCnt++;
+                    }
+                    n_cnt--;
+                }
+                i += 2;
+            } else {
+                n_cnt = 0;
+                i++;
+            }
+        }
+
+        System.out.println(ansCnt);
+    }
 }
