2be95dd5a48048df06d69edc679d76d109b134b6
hil679 <hil679922@gmail.com>
2025-03-25 18:22:40 +0900

    backtracking[양궁대회]: dfs, backtracking according to score - score + 1 until depth == n - for loop condition: start 0 to add same index

---
+++ src/main/java/org/example/programmers/dfs_bfs/backtracking/양궁대회.java
@@ -0,0 +1,49 @@
+package org.example.programmers.dfs_bfs.backtracking;
+
+public class 양궁대회 {
+    int max = -1;
+    int[] result = new int[11];//0~10점
+    int[] lion = new int[11];
+    public int[] solution(int n, int[] info) {
+        int[] answer = {-1};
+        dfs(0, n, info);
+        if (max == -1)
+            return answer;
+        return lion;
+    }
+
+    public void dfs(int depth, int n, int[] info) {
+        if (depth == n) {
+            int diff = score(info);
+            if (max <= diff) {
+                max = diff;
+                lion = result.clone();
+            }
+            return;
+        }
+
+        for (int i = 0; i < result.length && result[i] <= info[i]; i++) {
+            result[i]+=1;
+            dfs(depth+1, n, info);
+            result[i]-=1;
+        }
+    }
+
+    public int score(int[] info) {
+        int lion = 0;
+        int ap = 0;
+        for (int i =0; i < info.length; i++) {
+            if (info[i] == 0 && result[i] == 0) {
+                continue;
+            }
+            else if (info[i] < result[i]) {
+                lion += 10 -i;
+            } else if (info[i] >= result[i]) {
+                ap += 10 -i;
+            }
+        }
+        if (lion > ap)
+            return lion-ap;
+        return -1;
+    }
+}
