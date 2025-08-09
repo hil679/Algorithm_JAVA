9df1fcf68af86837ee6b981a586994d4a76aee22
hil679 <hil679922@gmail.com>
2025-08-09 22:52:14 +0900

    [no bfs] 이분그래프_bfs or dfs써야함,fail

---
+++ src/main/java/org/example/graph/P1707.java
@@ -0,0 +1,56 @@
+package org.example.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1707 {
+    
+
+    /*
+    1
+    5 4
+    1 2
+    3 4
+    2 3
+    4 1
+    간선 순서에 따라, 코드는 홀수 길이의 사이클을 발견하지 못하고 "YES"를 출력합니다.
+     */
+    public static void fail(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.valueOf(br.readLine());
+        while (n > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            int node[] = new int [V+1];
+            boolean flag = true;
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int color1 = Integer.parseInt(st.nextToken());
+                int color2 = Integer.parseInt(st.nextToken());
+
+
+                if (node[color1] == 0 && node[color2] == 0) {
+                    node[color1] = 1;
+                    node[color2] = -1;
+                } else if (node[color1] == 0) {
+                    node[color1] = -node[color2];
+                } else if (node[color2] == 0) {
+                    node[color2] = -node[color1];
+                } else if (node[color1] == node[color2]) {
+                    flag = false;
+                    break;
+                }
+                System.out.println(node[color1] + " " + node[color2]);
+
+            }
+            if (flag)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+            n--;
+        }
+    }
+}
