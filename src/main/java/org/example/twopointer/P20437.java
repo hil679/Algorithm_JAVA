25d0ecad1be26ff5df0ebc21a74073dd70d19edf
hil679 <hil679922@gmail.com>
2025-09-14 22:42:20 +0900

    [문자열 슬라이싱] 문자열 게임 2-fail

---
+++ src/main/java/org/example/twopointer/P20437.java
@@ -0,0 +1,48 @@
+package org.example.twopointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P20437 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String s = br.readLine();
+            int k = Integer.parseInt(br.readLine());
+
+            if(k == 1) {
+                // k가 1일때 -> 이거 없으면 틀림
+                // 아래에서 지금과 그 다음 문자부터 비교를 하기 때문에 1은 무조건 자기 자신임으로 따로 예외처리 필요
+                System.out.println("1 1");
+                continue;
+            }
+
+            int[] alpha = new int[26];//알파벳 별 개수를 저장한다.
+            for(int j = 0; j < s.length(); j++) {
+                alpha[s.charAt(j) - 'a']++;
+            }
+            int min = 10001;
+            int max = -1;
+
+            for(int j = 0; j < s.length(); j++) {
+                if(alpha[s.charAt(j) - 'a'] < k) continue;
+
+                int count = 1;
+                for(int l = j + 1; l < s.length(); l++) {
+                    if(s.charAt(j) == s.charAt(l)) count++;
+                    if(count == k) {
+                        min = Math.min(min, l - j + 1);
+                        max = Math.max(max, l - j + 1);
+                        break;
+                    }
+                }
+            }
+
+            if(min == 10001 || max == -1) System.out.println("-1");
+            else System.out.println(min + " " + max);
+
+        }
+    }
+}
