c8e6abda59ed75f935444228831dea0b1dc2076a
hil679 <hil679922@gmail.com>
2025-09-16 22:47:32 +0900

    [stack] 탑, with hashmap

--- src/main/java/org/example/stack/P2493.java
+++ src/main/java/org/example/stack/P2493.java
@@ -1,15 +1,50 @@
-package org.example.구현;
+package org.example.stack;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class P2493 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap <Integer, Integer> hm = new LinkedHashMap<>();
+        int i = 1;
+        while (st.hasMoreTokens()) {
+            Integer tower = Integer.parseInt(st.nextToken());
+            hm.put(tower, i);
+            i++;
+        }
+        int res[] = new int [n];
+        Stack<Integer> stack = new Stack<>();
+        //10 4 3 5 11
+        StringBuilder sb = new StringBuilder();
+        for (Integer key : hm.keySet()) {
+            if (stack.isEmpty()) {
+               sb.append(0);
+               sb.append(" ");
+            }
+            while (!stack.isEmpty()) {
+                int tower = stack.peek();
+                if (key < tower) {
+                    sb.append(hm.get(tower));
+                    sb.append(" ");
+                    break;
+                } else {
+                    stack.pop();
+                }
+                if(stack.isEmpty()) {
+                    sb.append(0);
+                    sb.append(" ");
+                }
+            }
+            stack.add(key);
+        }
+        System.out.println(sb.toString());
+    }
+    public static void prev1(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Integer n = Integer.valueOf(br.readLine());
         HashMap<Integer, Integer> towers = new LinkedHashMap<>();
